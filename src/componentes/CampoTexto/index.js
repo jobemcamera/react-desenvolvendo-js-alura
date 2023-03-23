d5cff298d55cd7c461cfd34a1bd1b5367fe6b93c
jobemcamera <jobecamera@gmail.com>
2023-03-23 19:47:13 -0300

    uso do useState

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,16 @@ const CampoTexto = (props) => {
     // props são os argumentos que são recebidos como parâmetro
     const placeholderModificado = `${props.placeholder}...`
 
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificado}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificado}/>
         </div>
     )
 }
