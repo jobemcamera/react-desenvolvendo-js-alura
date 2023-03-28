3887aa222d2e01edc94e8a3397f3080260fa1a94
jobemcamera <jobecamera@gmail.com>
2023-03-28 17:37:40 -0300

    criação do componente Colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/jobemcamera.png' alt='Jobe' />
+            </div>
+            <div className='rodape'>
+                <h4>Jobe</h4>
+                <h5>Front-End</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
