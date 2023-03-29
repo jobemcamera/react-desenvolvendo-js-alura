788f09e9286470da29edb6a7bea8415b5c598200
jobemcamera <jobecamera@gmail.com>
2023-03-29 16:11:20 -0300

    Componente Colaboradores passando o form com o filter

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,15 @@
 import './Colaborador.css'
 
-const Colaborador = (props) => {
+// Desestruturação das props = { nome, imagem, cargo }
+const Colaborador = ({nome, imagem, cargo}) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/jobemcamera.png' alt='Jobe' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>Jobe</h4>
-                <h5>Front-End</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,7 +6,15 @@ const Time = (props) => {
         // style in line assim como no HTML para pegar as props do array de objetos do time
         <section className='time' style={{backgroundColor: props.corSecundaria}}> 
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            <Colaborador />
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => 
+                    <Colaborador 
+                        nome={colaborador.nome} 
+                        cargo={colaborador.cargo} 
+                        imagem={colaborador.imagem}
+                    />
+                )}
+            </div>
         </section>
     )
 }
